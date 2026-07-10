3e1dc983c4e017dbdd0c6f0114d884b2ab66bfbd
missanshrestha-lang <missanshrestha@gmail.com>
2026-07-10 08:57:40 +0545

    Add monster battle game in kill_the_boss.py
    This script implements a text-based monster battle game where the player can choose a character and engage in combat with a boss named Superman. The game includes options to attack, heal, check status, or run away, with health management for both the player and the boss.

--- README.md
+++
@@ -1,1 +0,0 @@
-# Kill_The_Boss
---
+++ kill_the_boss.py
@@ -0,0 +1,97 @@
+import random
+while True:
+    health=200
+    print("="*90)
+    print(" ⚔️ MONSTER BATTLE ⚔️".center(90))
+    print("="*90)
+    print(' Defeat the Boss "SUPERMAN" to Save the Kingdom!'.center(90))
+    print("Kill The Boss Without Dying To Win".center(90))
+    boss_health=650
+    print("="*90)
+    character=["Goblin", "Skeleton", "Zombie", "Dragon"]
+    print("The characters are: ", character)
+    choice=input("Do you want to choose your character or let the computer choose for you? (y/n): ")
+    print()
+    if choice.upper()=="N":
+        random_character=random.choice(character)
+        print("The computer has chosen: ", random_character)
+    elif choice.upper()=="Y":
+        user_character=input("Which character do you want to choose?(1/2/3/4)? ")
+        print("You have chosen: ", character[int(user_character)-1])
+    else:
+        print("Invalid choice")
+        exit()
+    print("Health of your character is: ", health,"❤️")
+    print("-"*90)
+    print("The Boss has appeared!".center(90))
+    print(f"Boss Health: {boss_health}".center(90))
+    print("-"*90)
+    while health>=0 and boss_health>=0:
+        option=["Attack","Heal","your status","Run"]
+        for i in range(len(option)):
+            print(i+1, option[i])
+        move=input("What do you want to do? (1/2/3/4): ")
+        print("-"*90)
+        if move=="1":
+            boss_health-=52
+            print("You attacked the Superman!")
+            print("damage dealt: 52")
+            print(f"Superman's HP: {boss_health}")
+            print()
+            print("superman attacks you!")
+            health-=18
+            print(f"your health: {health}")
+            print("-"*90)
+            print(f"Boss Health: {boss_health}".center(90))
+            print("-"*90)
+        elif move=="2":
+            health+=50
+            print("You healed yourself!")
+            print(f"your health: {health}")
+            print("-"*90)
+        elif move=="3":
+            print("="*90)
+            print("YOUR STATUS".center(90))
+            print()
+            print(f"Health Of Your Character: {health}/200")   
+            print()
+            print()
+            print("BOSS STATUS".center(90))
+            print()
+            print("Boss: Superman")
+            print(f"health: {boss_health}/1000")
+            print("="*90)
+        elif move=="4":
+            print("You ran away from the battle!")
+            print()
+            print("#"*90)
+            print("GAME OVER".center(90))
+            print("#"*90)
+            print("You ran away from the battle and lost the game!")
+            print("Superman has taken over the kingdom and you have failed to save it!") 
+            print("-"*90)
+    if health<=0:
+        print()
+        print("#"*90)
+        print("GAME OVER".center(90))
+        print("#"*90)
+        print()
+        print("superman has defeated you and taken over the kingdom!")
+        print("You have failed to save the kingdom!")
+        print("Better luck next time!")
+    elif boss_health<=0:
+        print()
+        print("#"*90)
+        print("YOU WIN!".center(90))
+        print("#"*90)
+        print()
+        print("You have defeated the boss and saved the kingdom!")
+        print("Congratulations!")
+        print("You are a true hero!")
+        print("Thank you for playing the game!")
+    break
+    
+    
+
+
+
